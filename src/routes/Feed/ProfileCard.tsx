7455ef7190a62e942b08e6a8f16d539cead91dda
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 15:19:54 +0100

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -10,7 +10,7 @@ const ProfileCard: React.FC<Props> = () => {
   return (
     <StyledWrapper>
       <div className="title">
-        <Emoji>💻</Emoji> Profile
+        <Emoji>💻</Emoji> صاحب المدونة
       </div>
       <div className="content">
         <div className="top">
