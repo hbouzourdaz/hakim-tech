e2d9307917a249cf66b0239f518160f2dd3f9d37
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 15:10:56 +0100

    Update index.ts

--- src/assets/fonts/pretendard/index.ts
+++ src/assets/fonts/pretendard/index.ts
@@ -3,47 +3,47 @@ import localFont from 'next/font/local'
 export const pretendard = localFont({
   src: [
     {
-      path: './Pretendard-Thin.woff2',
+      path: './Tajawal-Light.ttf',
       weight: '100',
       style: 'normal',
     },
     {
-      path: './Pretendard-ExtraLight.woff2',
+      path: './Tajawal-ExtraLight.ttf',
       weight: '200',
       style: 'normal',
     },
     {
-      path: './Pretendard-Light.woff2',
+      path: './Tajawal-Light.ttf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: './Pretendard-Regular.woff2',
+      path: './Tajawal-Regular.ttf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './Pretendard-Medium.woff2',
+      path: './Tajawal-Medium.ttf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './Pretendard-SemiBold.woff2',
+      path: './Tajawal-SemiBold.ttf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: './Pretendard-Bold.woff2',
+      path: './Tajawal-Bold.ttf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: './Pretendard-ExtraBold.woff2',
+      path: './Tajawal-ExtraBold.ttf',
       weight: '800',
       style: 'normal',
     },
     {
-      path: './Pretendard-Black.woff2',
+      path: './Tajawal-Black.ttf',
       weight: '900',
       style: 'normal',
     },
