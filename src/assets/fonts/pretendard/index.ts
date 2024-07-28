17c4e1b648fefb9da1106b42780d4a3895f77287
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 22:02:36 +0100

    Update index.ts

--- src/assets/fonts/pretendard/index.ts
+++ src/assets/fonts/pretendard/index.ts
@@ -8,6 +8,11 @@ export const pretendard = localFont({
       style: 'normal',
     },
     {
+      path: './MiSansArabic-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
       path: './MiSansArabic-Light.ttf',
       weight: '300',
       style: 'normal',
@@ -22,13 +27,23 @@ export const pretendard = localFont({
       weight: '500',
       style: 'normal',
     },
+        {
+      path: './MiSansArabic-Semibold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
     {
       path: './MiSansArabic-Bold.ttf',
       weight: '700',
       style: 'normal',
     },
+       {
+      path: './MiSansArabic-Thin.ttf',
+      weight: '800',
+      style: 'normal',
+    },
     {
-      path: './MiSansArabic-Semibold.ttf',
+      path: './MiSansArabic-Heavy.ttf',
       weight: '900',
       style: 'normal',
     },
