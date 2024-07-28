5c279aa6cef6b9f85c2fa269cba8010df0ba3193
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 20:20:30 +0100

    Update index.tsx

--- src/layouts/RootLayout/ThemeProvider/Global/index.tsx
+++ src/layouts/RootLayout/ThemeProvider/Global/index.tsx
@@ -13,8 +13,8 @@ export const Global = () => {
           direction: rtl;
           margin: 0;
           padding: 0;
-          color: ${theme.colors.gray12};
-          background-color: ${theme.colors.gray1};
+          color: ${theme.colors.graydark};
+          background-color: ${theme.colors.gray};
           font-family: ${pretendard.style.fontFamily};
           font-weight: ${pretendard.style.fontWeight};
           font-style: ${pretendard.style.fontStyle};
