3fd7132d1a38d205fc3651fd6596321e2c8a0343
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 14:11:02 +0100

    Update index.ts

--- src/assets/fonts/tajawal /index.ts
+++ src/assets/fonts/tajawal /index.ts
@@ -1,7 +1,7 @@
 import { Tajawal } from 'next/font/google'
 
 export const tajawal = Tajawal({
-  weight: ['900'],
-  style: ['italic'],
-  subsets: ['latin'],
+  weight: ['900'], // Specify the weights you need
+  style: ['normal'], // Use only 'normal' since 'italic' is not supported
+  subsets: ['latin', 'arabic'], // Include Arabic subset for RTL support
 })
