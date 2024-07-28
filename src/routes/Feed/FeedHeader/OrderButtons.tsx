4873dbf1426dc5b578f8d96cd6dc1c82b4f66886
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 18:09:46 +0100

    Update OrderButtons.tsx

--- src/routes/Feed/FeedHeader/OrderButtons.tsx
+++ src/routes/Feed/FeedHeader/OrderButtons.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import { useRouter } from "next/router"
 import React from "react"
 
-type TOrder = "تصاعدي" | "تنازلي"
+type TOrder = "asc" | "desc"
 
 type Props = {}
 
