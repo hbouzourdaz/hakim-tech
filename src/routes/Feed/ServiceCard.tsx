16d57033760f57ef0c673d79daa6039692327bb5
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 15:22:29 +0100

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        <Emoji>🌟</Emoji> خدمة
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
