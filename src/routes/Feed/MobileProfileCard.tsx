2520a2f00c9deb57f3467ba41571b6d30d66c951
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 13:10:05 +0100

    Update MobileProfileCard.tsx

--- src/routes/Feed/MobileProfileCard.tsx
+++ src/routes/Feed/MobileProfileCard.tsx
@@ -34,41 +34,50 @@ const MobileProfileCard: React.FC<Props> = () => {
 export default MobileProfileCard
 
 const StyledWrapper = styled.div`
+  direction: rtl;
   display: block;
 
   @media (min-width: 1024px) {
     display: none;
+    direction: rtl;
   }
 
   > .top {
     padding: 0.25rem;
     margin-bottom: 0.75rem;
+    direction: rtl;
   }
   > .mid {
+    direction: rtl;
     padding: 0.5rem;
     margin-bottom: 1rem;
     border-radius: 1rem;
     background-color: ${({ theme }) =>
       theme.scheme === "light" ? "white" : theme.colors.gray4};
     > .wrapper {
+      direction: rtl;
       display: flex;
       gap: 0.5rem;
       align-items: center;
       > .wrapper {
+        direction: rtl;
         height: fit-content;
         > .top {
+          direction: rtl;
           font-size: 1.25rem;
           line-height: 1.75rem;
           font-style: italic;
           font-weight: 700;
         }
         > .mid {
+          direction: rtl;
           margin-bottom: 0.5rem;
           font-size: 0.875rem;
           line-height: 1.25rem;
           color: ${({ theme }) => theme.colors.gray11};
         }
         > .btm {
+          direction: rtl;
           font-size: 0.875rem;
           line-height: 1.25rem;
         }
